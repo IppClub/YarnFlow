3ae2d824f90af5782049f8952feaea1a5b077e5c
Li Jin <dragon-fly@qq.com>
2026-04-15 10:53:37 +0800

    Updated.

--- src/yarnflow/yarn_compiler.cpp
+++ src/yarnflow/yarn_compiler.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2016-2025 Li Jin <dragon-fly@qq.com>
+/* Copyright (c) 2016-2026 Li Jin <dragon-fly@qq.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -635,7 +635,7 @@ public:
 
 		Goto = key("jump") >> space >> (Title | node_name_error);
 
-		Call = not_((expr("endif") | "if" | "else" | "elseif" | "jump" | "set") >> not_alpha_num) >> Name >> space >> Seperator >> -(Exp >> *(space >> ',' >> space >> Exp));
+		Call = not_(key("endif") | key("if") | key("else") | key("elseif") | key("jump") | key("set")) >> Name >> space >> Seperator >> -(Exp >> *(space >> ',' >> space >> Exp));
 
 		Command = begin_command >> space >> (
 			If |
